4320c153dffac460fed3cb571bf49021b1e06518
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-09-19 07:55:29 +0100

    naive search for softwire.com

--- SharedExample/src/org/djna/email/EmailSearch.java
+++ SharedExample/src/org/djna/email/EmailSearch.java
@@ -5,10 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EmailSearch {
 
@@ -31,14 +32,30 @@ public class EmailSearch {
 
     private static void searchFile(String contents) {
 
-        String regex = "softwire.com";
+        String regex = "\\S+@((?:\\w+\\.)+\\w+)\\s*";
         Pattern emailPattern = Pattern.compile(regex);
         Matcher m = emailPattern.matcher(contents);
 
-        Map<String, Integer> domainMap = new HashMap<String, Integer>();
+        int matchCount = 0;
+        Set<String> uniqueDomains = new HashSet<>();
         while( m.find() ) {
-            System.out.println("got " + m);
+            String domain = m.group(1);
+            uniqueDomains.add(domain);
+            System.out.println("domain " + domain);
+            matchCount++;
         }
+
+        System.out.println("Found " + matchCount);
+        System.out.println("Count of unique domains " + uniqueDomains.size());
+
+        List<String> sortedDomains = uniqueDomains.stream().sorted().collect(Collectors.toList());
+        System.out.println("Sorted List of domains ");
+        for ( String domain: sortedDomains){
+            System.out.println("domain " + domain);
+        }
+
+
+
 
 
     }
